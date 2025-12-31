05ce7adde535d6cd8502cd0c7a4ee3e93b54a216
xooooooooox <xozoz.sos@gmail.com>
2026-01-01 02:15:07 +0800

    feat: init

--- src/main/shell/framework/bootstrap/context/completion.sh
+++ src/main/shell/framework/bootstrap/context/completion.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
 
-#######################################
-# 自动补全-全局变量
-# @see framework/context/vars/global_vars.sh
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-# Returns:
-#   None
-#######################################
-__framework_completion_vars() {
-  :
-}
-
 __main() {
-  __framework_completion_vars
+  :
 }
 
 __main
