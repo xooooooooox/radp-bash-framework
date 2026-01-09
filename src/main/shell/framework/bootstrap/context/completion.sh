412327821bf114e3338f4150a2f6a758e944716e
xooooooooox <xozoz.sos@gmail.com>
2026-01-09 12:39:41 +0800

    feat(completion): Optimize completion

--- src/main/shell/framework/bootstrap/context/completion.sh
+++ src/main/shell/framework/bootstrap/context/completion.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-__fw_completion_user_config() {
+__fw_setup_user_completion() {
+  # TODO v0.3.1-2026/1/9: 在下面两个方法中添加 shellcheck source=
   :
 }
 
-__fw_completion_user_lib() {
-  :
-}
-
-
+#----------------------------------------------------------------------------------------------------------------------#
 __main() {
-
-  # user
-  __fw_completion_user_config
-  __fw_completion_user_lib
+  __fw_setup_user_completion
 }
 
 __main
