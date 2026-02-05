5fd552cef7c37cf5da386dcba505b263a69568eb
xooooooooox <xozoz.sos@gmail.com>
2026-02-05 21:16:50 +0800

    fix: version.sh

--- src/main/shell/commands/version.sh
+++ src/main/shell/commands/version.sh
@@ -2,6 +2,9 @@
 # @cmd
 # @desc Show radp-bash-framework version
 
+# Application version - same as framework version
+declare -gr gr_app_version="v0.7.10"
+
 cmd_version() {
   radp_get_fw_install_version
 }
